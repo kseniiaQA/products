3e879211093eb056b2e059c0e8f69688b02d13a5
Your Name <you@example.com>
2021-06-23 18:49:34 +0300

    tests not covered 100%

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,71 @@
+package ru.netology.domain;
+
+import java.util.Objects;
+
+public class Smartphone extends Product {
+    private String manufacturer;
+//    private int pages;
+//    private int publishedYear;
+
+    public Smartphone() {
+        super();
+    }
+
+    public Smartphone(int id, String name, int price, String manufacturer) {
+        super(id, name, price);
+        this.manufacturer = manufacturer;
+//        this.pages = pages;
+//        this.publishedYear = publishedYear;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+//    public int getPages() {
+//        return pages;
+//    }
+//
+//    public void setPages(int pages) {
+//        this.pages = pages;
+//    }
+//
+//    public int getPublishedYear() {
+//        return publishedYear;
+//    }
+//
+//    public void setPublishedYear(int publishedYear) {
+//        this.publishedYear = publishedYear;
+//    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Smartphone book = (Smartphone) o;
+//        return pages == book.pages &&
+//                publishedYear == book.publishedYear &&
+        return Objects.equals(manufacturer, book.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+//        return Objects.hash(super.hashCode(), author, pages, publishedYear);
+        return Objects.hash(super.hashCode(), manufacturer);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "manufacturer='" + manufacturer + '\'' +
+//                ", pages=" + pages +
+//                ", publishedYear=" + publishedYear +
+                '}';
+    }
+}
---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,51 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+    ProductRepository productRepository = new ProductRepository();
+    ProductManager productManager = new ProductManager(productRepository);
+
+    Book book1 = new Book(0, "Война и мир", 132, "Лев Толстой");
+    Book book2 = new Book(1, "Приключения Нильса", 99, "Сельма Лагерлеф");
+    Smartphone smartphone1 = new Smartphone(2, "ВоСониПлюс", 31500, "Сони");
+    Smartphone smartphone2 = new Smartphone(3, "ТруПлэжа", 7200, "Самсунг");
+
+
+    @Test
+    void searchByMany() {
+        productManager.add(book1);
+        productManager.add(book2);
+        productManager.add(smartphone1);
+        productManager.add(smartphone2);
+
+
+        assertArrayEquals(new Product[]{book1,smartphone1},productManager.searchBy("Во"));
+    }
+
+    @Test
+    void searchByOneTrue() {
+        productManager.add(book1);
+
+        assertArrayEquals(new Product[]{book1},productManager.searchBy("Во"));
+    }
+
+    @Test
+    void searchByOneFalse() {
+        productManager.add(book2);
+
+        assertArrayEquals(new Product[]{},productManager.searchBy("Во"));
+    }
+
+    @Test
+    void searchByZero() {
+        assertArrayEquals(new Product[]{},productManager.searchBy("В"));
+    }
+}
