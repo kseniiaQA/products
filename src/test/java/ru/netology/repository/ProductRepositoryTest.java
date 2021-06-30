3e7579e3358eeb6465e6aa88324f8ba2acd7e196
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-06-30 18:33:27 +0300

    Update ProductRepositoryTest.java

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,56 +1,51 @@
 package ru.netology.repository;
 
-import org.junit.jupiter.api.Test;
-import ru.netology.domain.Book;
 import ru.netology.domain.Product;
-import ru.netology.domain.Smartphone;
 import ru.netology.exception.NotFoundException;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class ProductRepository {
 
-class ProductRepositoryTest {
-    private ProductRepository repository = new ProductRepository();
-    private Book coreJava = new Book();
-    Book book1 = new Book(0, "Война и мир", 132, "Лев Толстой");
-    Book book2 = new Book(1, "Приключения Нильса", 99, "Сельма Лагерлеф");
-    Smartphone smartphone1 = new Smartphone(2, "ВоСониПлюс", 31500, "Сони");
-    Smartphone smartphone2 = new Smartphone(3, "ТруПлэжа", 7200, "Самсунг");
-    @Test
-    public void shouldDeleteItem() {
-        repository.save(book1);
-        repository.save(book2);
-        repository.save(smartphone1);
-        repository.save(smartphone2);
+    private Product[] items = new Product[0];
 
-        repository.removeById(1);
-
-        assertArrayEquals(new Product[] {book1,smartphone1,smartphone2}, repository.findAll());
+    public void save(Product item) {
+        int length = items.length + 1;
+        Product[] tmp = new Product[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
     }
 
-    @Test
-    public void negativeId() {
-        repository.save(coreJava);
-
-        assertThrows(NotFoundException.class, () -> {
-            repository.removeById(-3);
-        });
+    public Product[] findAll() {
+        return items;
     }
 
-    @Test
-    public void outOfLength() {
-        repository.save(coreJava);
-//Этот тест наверное лишний. Повторяет предыдущий.
-        assertThrows(NotFoundException.class, () -> {
-            repository.removeById(10);
-        });
+    public Product findById(int id) {
+        for (Product item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
     }
 
-    @Test
-    public void shouldSaveAnItem() {
-        repository.save(coreJava);
+    public void removeById(int id) throws NotFoundException {
+        if (findById(id)==null) {
+            throw new NotFoundException("Элемент с id = "+id+" не найден!");
 
-        Product[] expected = new Product[]{coreJava};
-        Product[] actual = repository.findAll();
-        assertArrayEquals(expected, actual);
+
+        }
+        int length = items.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item : items) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        }
+        items = tmp;
     }
+
+
 }
